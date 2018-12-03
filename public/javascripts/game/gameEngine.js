b472451c31856315eccafb787502480cc65082b2
Tung Doan <doantungbk.203@gmail.com>
2018-12-03 17:53:16 +0100

    Minor

--- public/javascripts/game/gameEngine.js
+++ public/javascripts/game/gameEngine.js
@@ -43,6 +43,7 @@ require(['canvasPainter', 'playersManager', '../../sharedConstants'], function (
   function state_sync (currentTime, ellapsedTime) {
     // if (_gameState == enumState.Migrating)
   //  do something here - get state info
+  //    consider saving the data by the time frame
   }
 
   requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
